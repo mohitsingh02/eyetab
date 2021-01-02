88da83ba747e1eb7c1b0d488b643780114e7d3f6
Mohit singh <onetouch9917@gmail.com>
2021-01-02 13:25:31 +0530

    hi

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+$(document).ready(function() {
+   
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 56) {
+        $(".navbar").addClass("bg-dark");
+      } else {
+        $(".navbar").removeClass("bg-dark");
+      }
+    });
+    
+  });
